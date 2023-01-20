e5382db57295e38baa825fd84649e65c2c63bfff
Alex Prudhomme <prudhomme.alex42@gmail.com>
2023-01-19 21:32:33 -0500

    import bulma

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h1>Home</h1>
+    <button class="button">alex</button>
     <div>GLO-3102 Home</div>
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import { router } from "./router";
 import App from "./App.vue";
-
+import 'bulma/css/bulma.css';
 const app = createApp(App);
 
 app.use(router);

d7707de461259a19233cadb394720a0bde0f03ef
Roxmo56633 <dionneroxane@gmail.com>
2023-01-20 22:29:57 -0500

    add html Login

--- src/components/User.vue
+++ src/components/User.vue
@@ -5,4 +5,26 @@
   </div>
 </template>
 
-<style></style>
+<style>
+div {
+  margin: 3px;
+}
+label{
+  display: inline-block;
+  width: 100px;
+}
+</style>
+<form>
+  <div>
+  <label>Username: </label>
+  <input type="email" autocomplete="username" id="email" />
+  </div> 
+  <div>
+  <label>Password</label>
+  <input type="password" autocomplete="newPassword" />
+  </div>
+
+  <input type="submit" value="Login" />
+</form>
+
+

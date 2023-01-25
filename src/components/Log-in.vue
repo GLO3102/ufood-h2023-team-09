b994c04c0e477098b5cc828c78fd2734df9f9cbe
Alex Prudhomme <prudhomme.alex42@gmail.com>
2023-01-24 23:32:48 -0500

    fix resizing of login and signup pages

--- src/components/Log-in.vue
+++ src/components/Log-in.vue
@@ -11,40 +11,58 @@
 
 <template>
     <section class="hero">
-        <div class="hero-body">
-            <div class="container has-text-centered">
-                <div class="column is-4 is-offset-4">
-                    <h3 class="title has-text-black">Log in</h3>
-                    <hr class="login-hr">
-                    <p class="subtitle has-text-black">Please log in to proceed.</p>
-                    <div class="box">
-                        <figure class="avatar">
-                            <img src="@/assets/person.svg" width="55">
-                        </figure>
-                        <form>
-                            <div class="field">
-                                <div class="control has-icons-left">
-                                    <input class="input is-medium" type="email" placeholder="Email" autofocus="true">
-                                    <span class="icon is-small is-left">
-                                        <img src="@/assets/envelope.svg" width="20">
-                                    </span>
-                                </div>
-                                
+        <div class="hero-body has-text-centered">
+            <div class="login">
+                <h3 class="title has-text-black">Log in</h3>
+                <hr class="login-hr">
+                <p class="subtitle has-text-black">Please log in to proceed.</p>
+                <div class="box" id="loginBox">
+                    <figure class="avatar">
+                        <img src="@/assets/person.svg" width="55">
+                    </figure>
+                    <form>
+                        <div class="field">
+                            <div class="control has-icons-left">
+                                <input class="input is-medium" type="email" placeholder="Email" autofocus="true">
+                                <span class="icon is-small is-left">
+                                    <img src="@/assets/envelope.svg" width="20">
+                                </span>
                             </div>
-
-                            <div class="field">
-                                <div class="control has-icons-left">
-                                    <input class="input is-medium" type="password" placeholder="Password">
-                                    <span class="icon is-small is-left">
-                                        <img src="@/assets/lock.svg" width="20">
-                                    </span>
-                                </div>
+                        </div>
+                        <div class="field">
+                            <div class="control has-icons-left">
+                                <input class="input is-medium" type="password" placeholder="Password">
+                                <span class="icon is-small is-left">
+                                    <img src="@/assets/lock.svg" width="20">
+                                </span>
                             </div>
-                            <button class="button is-block is-info is-medium is-fullwidth" @click="login()">Login</button>
-                        </form>
-                    </div>
+                        </div>
+                        <button class="button is-block is-info is-medium is-fullwidth" @click="login()">Login</button>
+                    </form>
                 </div>
             </div>
         </div>
     </section>
 </template>
+
+<style scoped>
+
+#loginBox {
+    min-width: 400px;
+    max-width: 800px;
+    margin: 0 auto;
+}
+.hero-body {
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+}
+.login {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    padding: 1.5rem;
+}
+</style>
--- src/components/Sign-up.vue
+++ src/components/Sign-up.vue
@@ -11,44 +11,64 @@
 
 <template>
     <section class="hero">
-        <div class="hero-body">
-            <div class="container has-text-centered">
-                <div class="column is-4 is-offset-4">
-                    <h3 class="title has-text-black">Sign up</h3>
-                    <hr class="login-hr">
-                    <p class="subtitle has-text-black">Please sign up to proceed.</p>
-                    <div class="box">
-                        <form>
-                            <div class="field">
-                                <div class="control has-icons-left">
-                                    <input class="input is-medium" type="username" placeholder="Username" autofocus="true">
-                                    <span class="icon is-small is-left">
-                                        <img src="@/assets/user.svg" width="20">
-                                    </span>
-                                </div>
+        <div class="hero-body has-text-centered">
+            <div class="login">
+                <h3 class="title has-text-black">Sign up</h3>
+                <hr class="login-hr">
+                <p class="subtitle has-text-black">Please sign up to proceed.</p>
+                <div class="box" id="loginBox">
+                    <form>
+                        <div class="field">
+                            <div class="control has-icons-left">
+                                <input class="input is-medium" type="username" placeholder="Username" autofocus="true">
+                                <span class="icon is-small is-left">
+                                    <img src="@/assets/user.svg" width="20">
+                                </span>
                             </div>
-                            <div class="field">
-                                <div class="control has-icons-left">
-                                    <input class="input is-medium" type="email" placeholder="Email">
-                                    <span class="icon is-small is-left">
-                                        <img src="@/assets/envelope.svg" width="20">
-                                    </span>
-                                </div>
+                        </div>
+                        <div class="field">
+                            <div class="control has-icons-left">
+                                <input class="input is-medium" type="email" placeholder="Email">
+                                <span class="icon is-small is-left">
+                                    <img src="@/assets/envelope.svg" width="20">
+                                </span>
                             </div>
+                        </div>
 
-                            <div class="field">
-                                <div class="control has-icons-left">
-                                    <input class="input is-medium" type="password" placeholder="Password">
-                                    <span class="icon is-small is-left">
-                                        <img src="@/assets/lock.svg" width="20">
-                                    </span>
-                                </div>
+                        <div class="field">
+                            <div class="control has-icons-left">
+                                <input class="input is-medium" type="password" placeholder="Password">
+                                <span class="icon is-small is-left">
+                                    <img src="@/assets/lock.svg" width="20">
+                                </span>
                             </div>
-                            <button class="button is-block is-info is-medium is-fullwidth" @click="signUp()">Sign up</button>
-                        </form>
-                    </div>
+                        </div>
+                        <button class="button is-block is-info is-medium is-fullwidth" @click="signUp()">Sign up</button>
+                    </form>
                 </div>
             </div>
         </div>
     </section>
 </template>
+
+<style scoped>
+
+#loginBox {
+    min-width: 400px;
+    max-width: 800px;
+    margin: 0 auto;
+}
+.hero-body {
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+}
+.login {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    padding: 1.5rem;
+}
+</style>

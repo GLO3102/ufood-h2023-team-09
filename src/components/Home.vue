a6dece9c38555b4d9eca15e80052d0eb8bd11f4a
Felix Prevost <felix_prevost@hotmail.com>
2023-01-26 00:58:30 -0500

    Update Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,73 +1,99 @@
-<script>
+<script setup>
 function dropDownOnOff() {
   let dropdown = document.getElementById("dropdownCategories");
   dropdown.classList.toggle("is-active");
 }
+</script>
 
-import RestoCard from "./RestoCard.vue";
+<script>
+import RestaurantCard from "./RestaurantCard.vue";
 export default {
   name: "app",
-  components: { "resto-card": RestoCard },
+  components: { "restaurant-card": RestaurantCard },
 };
 </script>
 
-
-
 <template>
   <div>
-    <h1>Restaurants</h1>
-
-    <input class="input" type="search" placeholder="Search..." />
-    <div class="dropdown" id="dropdownCategories" @click="dropDownOnOff()">
-      <div class="dropdown-trigger">
-        <button
-          class="button"
-          aria-haspopup="true"
-          aria-controls="dropdown-menu"
-        >
-          <span>Categories</span>
-          <span class="icon is-small">
-            <i class="fas fa-angle-down" aria-hidden="true"></i>
-          </span>
-        </button>
+    <div class="search-filter">
+      <div class="search">
+        <input class="input" type="search" placeholder="Search..." />
       </div>
-      <div class="dropdown-menu" id="dropdown-menu" role="menu">
-        <div class="dropdown-content">
-          <a href="#" class="dropdown-item"> Fast Food </a>
-          <a class="dropdown-item"> Indian </a>
-          <a href="#" class="dropdown-item is-active"> Asian </a>
-          <a href="#" class="dropdown-item"> Pizza </a>
-          <!-- <hr class="dropdown-divider" /> -->
-          <a href="#" class="dropdown-item"> Middle Eastern </a>
+
+      <div class="filter">
+        <div class="dropdown" id="dropdownCategories" @click="dropDownOnOff()">
+          <div class="dropdown-trigger">
+            <button
+              class="button"
+              aria-haspopup="true"
+              aria-controls="dropdown-menu"
+            >
+              <span>Categories</span>
+              <span class="icon is-small">
+                <i class="fas fa-angle-down" aria-hidden="true"></i>
+              </span>
+            </button>
+          </div>
+          <div class="dropdown-menu" id="dropdown-menu" role="menu">
+            <div class="dropdown-content">
+              <a href="#" class="dropdown-item"> Fast Food </a>
+              <a class="dropdown-item"> Indian </a>
+              <a href="#" class="dropdown-item is-active"> Asian </a>
+              <a href="#" class="dropdown-item"> Pizza </a>
+              <!-- <hr class="dropdown-divider" /> -->
+              <a href="#" class="dropdown-item"> Middle Eastern </a>
+            </div>
+          </div>
         </div>
+
+        <button class="button">$</button>
+        <button class="button">$$</button>
+        <button class="button">$$$</button>
+        <button class="button">$$$$</button>
       </div>
     </div>
 
-    <button class="button">$</button>
-
-    <button class="button">$$</button>
-    <button class="button">$$$</button>
-
-    <div class="resto-list">
-      <resto-card></resto-card>
-      <resto-card></resto-card>
-      <resto-card></resto-card>
-      <resto-card></resto-card>
-      <resto-card></resto-card>
-      <resto-card></resto-card>
-      <resto-card></resto-card>
-      <resto-card></resto-card>
-      <resto-card></resto-card>
-      <resto-card></resto-card>
+    <div class="restaurant-list">
+      <div class="restaurant-card"><restaurant-card></restaurant-card></div>
+      <div class="restaurant-card"><restaurant-card></restaurant-card></div>
+      <div class="restaurant-card"><restaurant-card></restaurant-card></div>
+      <div class="restaurant-card"><restaurant-card></restaurant-card></div>
+      <div class="restaurant-card"><restaurant-card></restaurant-card></div>
+      <div class="restaurant-card"><restaurant-card></restaurant-card></div>
+      <div class="restaurant-card"><restaurant-card></restaurant-card></div>
+      <div class="restaurant-card"><restaurant-card></restaurant-card></div>
+      <div class="restaurant-card"><restaurant-card></restaurant-card></div>
     </div>
-    
 
     <div>GLO-3102 Home</div>
   </div>
 </template>
 
-<style>
-resto-card{
-  width: 280px;
+<style scoped>
+.search-filter {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  margin-bottom: 3dvw;
+}
+.search {
+  width: 500px;
+  margin-top: 3dvw;
+}
+.filter {
+  margin-top: 3dvw;
+  display: flex;
+  flex-wrap: nowrap;
+}
+
+.restaurant-list {
+  display: flex;
+  flex-wrap: wrap;
+  margin-inline: 3dvw;
+  justify-content: center;
+}
+.restaurant-card {
+  width: 380px;
+  margin: 1dvw;
 }
 </style>

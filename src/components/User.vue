0e6750c5ea7ce9b8639e7d05c4a36c4db5755f75
Roxmo56633 <dionneroxane@gmail.com>
2023-01-29 22:00:24 -0500

    userViewLookingQuiteGood

--- src/components/User.vue
+++ src/components/User.vue
@@ -18,10 +18,11 @@ export default defineComponent({
 <template>
   <div class="hero">
     <div class="hero-body">
-      <div class="container has-text-centered">
-        <div class="columns is-vcentered">
-          <div class="column is-5">
-            <figure class="image is-4by4">
+      <!-- Begin user info-->
+      <div>
+        <nav class="level">
+          <div class="level-item has-text-centered">
+          <figure class="image is-4by4">
               <img
                 class="is-rounded"
                 src="https://i.pinimg.com/originals/05/11/45/051145a8e366876f859378154aa7df8b.jpg"
@@ -29,54 +30,113 @@ export default defineComponent({
               />
             </figure>
           </div>
-          <div class="column is-6 is-offset-1">
-            <h1 class="title is-2">Superhero Scaffolding</h1>
+          
+            <div class="level-item">
+            <div class="is-6 is-offset-32 has-text-centered">
+            <h1 class="title is-2">User Name<br></h1>
             <h2 class="subtitle is-4">
-              Let this cover page describe a product or service.
+              <br>This is user' score
             </h2>
-            <br />
-            <p class="has-text-centered">
-              <a class="button is-medium is-info is-outlined"> Learn more </a>
-            </p>
           </div>
         </div>
+        </nav> 
       </div>
-      <!-- Begin Work Content -->
+      <!-- End user info-->
+      <!-- Begin Work Content IF USER HAVE VISITED RESTAURANTS-->
       <Carousel wrap-around="true" :items-to-show="2.5">
-          <Slide :key="slide">
-            <div class="carousel__item">
-              <div class="card">
-                <figure class="image is-3by5">
-                  <img
-                    src="https://i.pinimg.com/564x/c5/1a/41/c51a41fad6717db9106f0f2a89a2ea00.jpg"
-                    alt=""
-                  />
-                </figure>
-              </div>
-              <div class="card-content slider-text">
-                <div class="is-size-5 box">IMAGE_CAPTION_HERE</div>
-              </div>
+        <Slide :key="slide">
+          <div class="carousel__item">
+            <div class="card">
+              <figure class="image is-3by5">
+                <img
+                  src="https://i.pinimg.com/564x/c5/1a/41/c51a41fad6717db9106f0f2a89a2ea00.jpg"
+                  alt=""/>
+              </figure>
             </div>
-          </Slide>
-          <Slide :key="slide">
-            <div class="carousel__item">2</div>
-          </Slide>
-          <Slide :key="slide">
-            <div class="carousel__item">3</div>
-          </Slide>
-          <Slide :key="slide">
-            <div class="carousel__item">4</div>
-          </Slide>
-          <Slide :key="slide">
-            <div class="carousel__item">5</div>
-          </Slide>
-
-          <template #addons>
-            <Navigation />
-            <Pagination />
-          </template>
-        </Carousel>
-        <!-- End Work Content -->
+            <div class="card-content is-overlay">
+              <span class="tag is-primary">Number of visits</span>
+            </div>
+            <div class="card-content slider-text">
+              <div class="is-size-5 box">IMAGE_CAPTION_HERE</div>
+            </div>
+          </div>
+        </Slide>
+        <Slide :key="slide">
+          <div class="carousel__item">
+            <div class="card">
+              <figure class="image is-3by5">
+                <img
+                  src="https://i.pinimg.com/564x/08/25/38/082538afc45124898e4a792a5250d352.jpg"
+                  alt=""/>
+              </figure>
+            </div>
+            <div class="card-content is-overlay">
+              <span class="tag is-primary">Number of visits</span>
+            </div>
+            <div class="card-content slider-text">
+              <div class="is-size-5 box">IMAGE_CAPTION_HERE</div>
+            </div>
+          </div>
+        </Slide>
+        <Slide :key="slide">
+          <div class="carousel__item">
+            <div class="card">
+              <figure class="image is-3by5">
+                <img
+                  src="https://i.pinimg.com/564x/95/19/fb/9519fb6c2a9576b4017356c29fb15f93.jpg"
+                  alt=""/>
+              </figure>
+            </div>
+            <div class="card-content is-overlay">
+              <span class="tag is-primary">Number of visits</span>
+            </div>
+            <div class="card-content slider-text">
+              <div class="is-size-5 box">IMAGE_CAPTION_HERE</div>
+            </div>
+          </div>
+        </Slide>
+        <Slide :key="slide">
+          <div class="carousel__item">
+            <div class="card">
+              <figure class="image is-3by5">
+                <img
+                  src="https://i.pinimg.com/564x/9f/2f/6b/9f2f6bd333752f91e23514ea1a946e3c.jpg"
+                  alt=""/>
+              </figure>
+            </div>
+            <div class="card-content is-overlay">
+              <span class="tag is-primary">Number of visits</span>
+            </div>
+            <div class="card-content slider-text">
+              <div class="is-size-5 box">IMAGE_CAPTION_HERE</div>
+            </div>
+          </div>
+        </Slide>
+        <Slide :key="slide">
+          <div class="carousel__item">
+            <div class="card">
+              <figure class="image is-3by5">
+                <img
+                  src="https://i.pinimg.com/564x/98/10/2c/98102cbaf0d75501f968ece02a83482f.jpg"
+                  alt=""/>
+              </figure>
+            </div>
+            <div class="card-content is-overlay">
+              <span class="tag is-primary">Number of visits</span>
+            </div>
+            <div class="card-content slider-text">
+              <div class="is-size-5 box">IMAGE_CAPTION_HERE</div>
+            </div>
+          </div>
+        </Slide>
+        <template #addons>
+          <Navigation />
+          <Pagination />
+        </template>
+      </Carousel>
+      <!-- End Work Content IF USER HAVE VISITED RESTAURANTS-->
+      <!-- Begin Work Content IF USER DONT VISIT//GOTTA SHOW INDICATION OF NON VISITED AND LINK TO HOME PAGE-->
+      <!-- End Work Content IF USER DONT VISIT-->
     </div>
     <div class="hero-foot">
       <div class="container">

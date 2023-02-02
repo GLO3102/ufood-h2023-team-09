e686753f1a87281723812344c64433ae7ebfc21c
Felix Prevost <felix_prevost@hotmail.com>
2023-02-01 20:34:45 -0500

    DynamicCards

--- src/components/RestaurantCard.vue
+++ src/components/RestaurantCard.vue
@@ -1,38 +1,37 @@
 <script setup>
+let today = new Date().getDay();
 </script>
 
 <template>
   <div class="card">
     <div class="card-image">
       <figure class="image is-4by3">
-        <img
-          src="https://bulma.io/images/placeholders/1280x960.png"
-          alt="Placeholder image"
-        />
+        <img :src="pictures" alt="Placeholder image" />
       </figure>
     </div>
     <div class="card-content">
       <div class="media">
-        <div class="media-left">
-          <figure class="image is-48x48">
-            <img
-              src="https://bulma.io/images/placeholders/96x96.png"
-              alt="Placeholder image"
-            />
-          </figure>
-        </div>
+        <div class="media-left"></div>
         <div class="media-content">
-          <p class="title is-4">{{name}}</p>
-          <p class="subtitle is-6">@johnsmith</p>
+          <p class="title is-4">{{ name }}</p>
+          <div class="genres" v-for="genre in genres" :key="genre">
+            {{ genre }}
+          </div>
         </div>
       </div>
-
+      <!-- <div>{{ opening_hours }}</div> -->
+      <div v-if="today === 1">Today : {{ monday }}(monday)</div>
+      <div v-else-if="today === 2">Today : {{ tuesday }}(tuesday)</div>
+      <div v-else-if="today === 3">Today : {{ wednesday }}(wednesday)</div>
+      <div v-else-if="today === 4">Today : {{ thursday }}(thursday)</div>
+      <div v-else-if="today === 5">Today : {{ friday }}(friday)</div>
+      <div v-else-if="today === 6">Today : {{ saturday }}(saturday)</div>
+      <div v-else>Today : {{ sunday }}(sunday)</div>
+      <div>{{ address }}</div>
+      <div>{{ tel }}</div>
+      <div>Price range : {{ price_range }}</div>
       <div class="content">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec
-        iaculis mauris. <a>@bulmaio</a>. <a href="#">#css</a>
-        <a href="#">#responsive</a>
-        <br />
-        <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+        <p>Cote : {{ rating }}</p>
       </div>
     </div>
   </div>
@@ -40,8 +39,34 @@
 
 <script>
 export default {
-  props: ['name']
-}
+  props: [
+    "name",
+    "id",
+    "address",
+    "tel",
+    "location",
+    "opening_hours",
+    "monday",
+    "tuesday",
+    "wednesday",
+    "thursday",
+    "friday",
+    "saturday",
+    "sunday",
+    "pictures",
+    "genres",
+    "price_range",
+    "rating",
+  ],
+};
 </script>
+
+<style scoped>
+.genres {
+  display: inline;
+  padding-inline: 2px;
+  border: solid;
+}
+</style>
 
 

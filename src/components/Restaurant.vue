9486bb819c561038885f31f72c3b07ea5880a771
Samuel Côté <122579295+cnator1@users.noreply.github.com>
2023-02-02 23:41:57 -0500

    Added First draft for restaurant layout (#12)
    * schedule better looking
    
    * Features pour Restaurant
    
    * Restaurant first layout
    
    * rebase
    
    * rebase 2
    
    ---------
    
    Co-authored-by: NicManzaUL <nicholas.manzano.1@ulaval.ca>

--- src/components/Restaurant.vue
+++ src/components/Restaurant.vue
@@ -1,16 +1,10 @@
 <template>
-  <div class = "container">
-    {{resto_data.name}}
-    <div>
-      {{ resto_data.address }}
-    </div>
-    <div>
-      {{ resto_data.location }}
-    </div>
-    <div>
-      {{ resto_data.tel }}
-    </div>
-    <div class="rating-container">
+  <div class="resto">
+  <div class="container is-max-widescreen">
+    <br><br>
+      <div class="box">
+        <h1 class="title">{{resto_data.name}}</h1>
+        <div class="rating-container">
       <svg style="display:none;">
         <defs>
           <symbol id="fivestars">
@@ -22,29 +16,38 @@
           </symbol>
         </defs>
       </svg>
-      <div class="rating">
-      <!--   <div class="rating-bg" style="width: 90%;"></div> -->
-        <progress class="rating-bg" value="0" max="5"></progress>
-        <svg><use xlink:href="#fivestars"/></svg>
+      <div class="evaluation">
+          <div class="rating">
+        <!--   <div class="rating-bg" style="width: 90%;"></div> -->
+          <progress class="rating-bg" :value="resto_data.rating" max="5"></progress>
+          <svg><use xlink:href="#fivestars"/></svg> 
+        </div>
+        <span class="tag is-warning">{{resto_data.rating}}</span>
+        <span :class="'tag is-' + color_map[resto_data.price_range-1]">
+        {{ "$".repeat(resto_data.price_range) }}</span>
       </div>
+     
     </div>
-
-    <div class="schedule" id="schedule">
-      <IloveWeb></IloveWeb>
     </div>
 
     <div class="box">
-          <!--carousel title-->
-          <div class="has-text-centered">
-            <div>
-              <h1 class="title is-3" >Menu                     
-<br /></h1>
-            </div>
-          </div>
-    <div class="slider_container">
-      <Carousel id="gallery" :items-to-show="1" :wrap-around="true" v-model="currentSlide"
-            :settings="settings"
-            :breakpoints="breakpoints">
+      <div class="column one-half">
+        <h1 class="title">About us</h1>
+        <Schedule></Schedule>
+      </div>
+    </div>
+
+    <div>
+      {{ resto_data.location }}
+    </div>
+    <div>
+      {{ resto_data.tel }}
+    </div>
+
+
+    <MapLocation></MapLocation>
+    <Carousel id="gallery" :items-to-show="1" :wrap-around="true" v-model="currentSlide"
+            :settings="settings">
             <Slide  :key="slide">
         <div class="carousel__item">
                 <div class="card">
@@ -60,7 +63,7 @@
       <Slide  :key="slide">
         <div class="carousel__item">
                 <div class="card">
-                  <figure >
+                  <figure>
                     <img
                       src="https://offloadmedia.feverup.com/secretlosangeles.com/wp-content/uploads/2019/10/22095522/70991088_150912036128545_2481090655335180758_n.jpg"
                       alt=""
@@ -72,7 +75,7 @@
       <Slide :key="slide">
         <div class="carousel__item">
                 <div class="card">
-                  <figure >
+                  <figure>
                     <img
                       src="http://www.lphishiring.com/assets/culture-1.jpg"
                       alt=""
@@ -82,47 +85,153 @@
           </div>
       </Slide>
       </Carousel>
-
+    
       <Carousel
-        id="thumbnails"
-        :items-to-show="2"
-        :wrap-around="true"
-        v-model="currentSlide"
-        ref="carousel">
-        <Slide v-for="slide in resto_data.pictures.length" :key="slide">
-          <div class="carousel__item" @click="slideTo(slide - 1)">{{ slide }}</div>
-        </Slide>
-      </Carousel>
+            wrap-around="true"
+            :settings="settings"
+            :breakpoints="breakpoints"
+          >
+            <Slide :key="slide">
+              <div class="carousel__item">
+                <div class="card">
+                  <figure class="image is-4by3">
+                    <img
+                      src="https://i.pinimg.com/236x/83/c1/e7/83c1e7c64211f263f588a2f74dd309c6.jpg"
+                      alt=""
+                    />
+                  </figure>
+                </div>
+                <div class="card-content is-overlay">
+                  <span class="tag is-primary is-size-5">20</span>
+                </div>
+                <div class="card-content slider-text">
+                  <div class="is-size-5 box">RESTAURANT_NAME</div>
+                </div>
+              </div>
+            </Slide>
+            <Slide :key="slide">
+              <div class="carousel__item">
+                <div class="card">
+                  <figure class="image is-4by3">
+                    <img
+                      src="https://i.pinimg.com/564x/08/25/38/082538afc45124898e4a792a5250d352.jpg"
+                      alt=""
+                    />
+                  </figure>
+                </div>
+                <div class="card-content is-overlay">
+                  <span class="tag is-primary is-size-5">9</span>
+                </div>
+                <div class="card-content slider-text">
+                  <div class="is-size-5 box">RESTAURANT_NAME</div>
+                </div>
+              </div>
+            </Slide>
+            <Slide :key="slide">
+              <div class="carousel__item">
+                <div class="card">
+                  <figure class="image is-4by3">
+                    <img
+                      src="https://i.pinimg.com/564x/95/19/fb/9519fb6c2a9576b4017356c29fb15f93.jpg"
+                      alt=""
+                    />
+                  </figure>
+                </div>
+                <div class="card-content is-overlay">
+                  <span class="tag is-primary is-size-5">15</span>
+                </div>
+                <div class="card-content slider-text">
+                  <div class="is-size-5 box">RESTAURANT_NAME</div>
+                </div>
+              </div>
+            </Slide>
+            <Slide :key="slide">
+              <div class="carousel__item">
+                <div class="card">
+                  <figure class="image is-4by3">
+                    <img
+                      src="https://i.pinimg.com/564x/9f/2f/6b/9f2f6bd333752f91e23514ea1a946e3c.jpg"
+                      alt=""
+                    />
+                  </figure>
+                </div>
+                <div class="card-content is-overlay">
+                  <span class="tag is-primary is-size-5">10</span>
+                </div>
+                <div class="card-content slider-text">
+                  <div class="is-size-5 box">RESTAURANT_NAME</div>
+                </div>
+              </div>
+            </Slide>
+            <Slide :key="slide">
+              <div class="carousel__item">
+                <div class="card">
+                  <figure class="image is-4by3">
+                    <img
+                      src="https://i.pinimg.com/564x/98/10/2c/98102cbaf0d75501f968ece02a83482f.jpg"
+                      alt=""
+                    />
+                  </figure>
+                </div>
+                <div class="card-content is-overlay">
+                  <span class="tag is-primary is-size-5">8</span>
+                </div>
+                <div class="card-content slider-text">
+                  <div class="is-size-5 box">RESTAURANT_NAME</div>
+                </div>
+              </div>
+            </Slide>
+ 
+            <template #addons>
+              <Navigation />
+              <Pagination />
+            </template>
+          </Carousel>
     </div>
     </div> 
-  </div>
+
 </template>
 
 <script setup>
-  import { onMounted } from 'vue';
-  const hours = json.opening_hours;
+  const color_map = ["black", "warning", "primary"];
 
-  onMounted((hours) => {
-    console.log("Mounted");
-    formatSchedule1(hours);
-  })
+ 
 
-  const formatSchedule1 = (hours) => {
-    let container1 = document.getElementById("schedule");
-    let table = document.createElement("table");
-    for (let key in hours){
-      let table_element = document.createElement("tr");
-      table_element.textContent = key + "->" + hours[key];
-      table.appendChild(table_element);
-    }
-    container1.appendChild(table);
-    
-  }
+
+const settings = {
+  itemsToShow: 1,
+};
+
+const breakpoints = {
+  640: {
+    itemsToShow: 1.5,
+  },
+
+  750: {
+    itemsToShow: 1.8,
+  },
+
+  1000: {
+    itemsToShow: 2,
+  },
+  1200: {
+    itemsToShow: 3,
+  },
+
+  1300: {
+    itemsToShow: 3.5,
+  },
+  1500: {
+    itemsToShow: 4.5,
+  },
+};
+
 </script>
 <script>
   import { defineComponent } from 'vue';
   import { Carousel, Slide } from 'vue3-carousel';
   import  Schedule  from './Schedule.vue';
+  import MapLocation from './MapLocation.vue';
   import json from './hardcoded_resto.json';
   import 'vue3-carousel/dist/carousel.css';
   
@@ -133,7 +242,7 @@
     components: {
       Carousel,
       Slide,
-      "IloveWeb" : Schedule
+      MapLocation,
     },
     data: () => ({
       currentSlide: 0,
@@ -142,23 +251,27 @@
     methods: {
       slideTo(val) {
         this.currentSlide = val;
-      },
-      
-      formatSchedule(array_json){
-        let container1 = document.getElementById("schedule");
-        let table = document.createElement("table");
-        for (let key in array_json){
-          let table_element = document.createElement("tr");
-          table_element.textContent = key + "->" + array_json[key];
-          table.appendChild(table_element);
-        }
-        container1.appendChild(table);
       }
     }
   });
 </script>
 
 <style scoped>
+span + span {
+    margin-left: 10px;
+}
+.resto{
+  background: #fff;
+  box-shadow: inset 100px 0px 100px -50px #959595, 
+              inset -100px 0px 100px -50px #959595;
+}
+.container{
+
+  margin: auto;
+}
+.image img {
+  object-fit: cover;
+}
 .slider_container{
   width: 100%;
   margin: auto;
@@ -171,27 +284,67 @@
   color: white;
   font-size: 20px;
   border-radius: 8px;
+}
+.carousel {
+  position: relative;
+
   display: flex;
   justify-content: center;
-  align-items: center;
+  flex-direction: column;
+
+}
+.carousel__item {
+  position: relative;
+
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  
+
 }
 
-.carousel__slide {
-  padding: 10px;
-}
 
-.carousel__prev,
-.carousel__next {
+.carousel__prev{
   box-sizing: content-box;
   border: 5px solid white;
 }
+.evaluation{
+  display: flex;
+}
 </style>
 <style>
+.carousel__slide {
+  padding: 25px;
+  opacity: 0.25;
+  transform: scale(0.8);
+
+
+}
+.carousel__slide--prev {
+
+  opacity: 0.5;
+
+}
+.carousel__slide--next {
+
+  opacity: 0.5;
+
+}
+.carousel__slide--active {
+
+  opacity: 1.0;
+  transform: scale(1);
+}
+
+.evaluation{
+  display: flex;
+}
+
 .rating {
   width: 120px;
   height: 24px;
   position: relative;
-  background-color: gray;
+  margin-right: 10px;
 }
 
 .rating progress.rating-bg {
--- src/components/Schedule.vue
+++ src/components/Schedule.vue
@@ -1,32 +1,27 @@
-<script>
+<script setup>
     import json from "./hardcoded_resto.json";
-    
-    export default{
-        name: "Schedule",
-        data: () => ({
-            currentSlide: 0,
-            resto_data : json,
-            week_day : [ "monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
-
-            })
-    }
+    json = json.items[0];
 </script>
 
 <template>
     <div>
-        <div class="schedule-container" style="width: 200px; float: left;">
-            <table  v-for="(hour, day) in resto_data.opening_hours" :key="day">
-                <tr  >{{day}} :{{hour}}</tr>   
+        <div class="box" style="width: 250px;">
+            <table class="table is-hoverable">
+                <thead>
+                    <th><abbr title="Weekday">Weekday</abbr></th>
+                    <th><abbr title="Opening hours">Opened</abbr></th>
+                </thead>
+                <tbody v-for="(hour, day) in json.opening_hours" :key="day">
+                    <tr>
+                        <th> {{day}} </th>
+                        <td> {{hour}}</td>
+                    </tr>   
+                </tbody>
             </table>
         </div>
-        <div class="card" style="margin-left: 300px;">
-            <figure >
-                <img
-                    src="https://media.wired.com/photos/59269cd37034dc5f91bec0f1/191:100/w_1280,c_limit/GoogleMapTA.jpg"
-                  alt=""
-                  style="width:auto;height:auto;"
-                />
-            </figure>
-        </div>
+
     </div>
 </template>
+
+<style scoped>
+</style>

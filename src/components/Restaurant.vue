fc60d43004a6977f94d36f6ef6b26d1b4cfbddd9
Alex Prudhomme <prudhomme.alex42@gmail.com>
2023-02-05 10:14:41 -0500

    made resto-info responsive

--- src/components/Restaurant.vue
+++ src/components/Restaurant.vue
@@ -1,61 +1,78 @@
 <template>
   <div class="resto">
     <div class="container is-max-widescreen">
-      <br><br>
+      <br /><br />
       <div class="box">
         <h1 class="title">{{ resto_data.name }}</h1>
         <div class="rating-container">
-          <svg style="display:none;">
+          <svg style="display: none">
             <defs>
               <symbol id="fivestars">
                 <path
                   d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z M0 0 h24 v24 h-24 v-24"
-                  fill="white" fill-rule="evenodd" />
+                  fill="white"
+                  fill-rule="evenodd"
+                />
                 <path
                   d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z M0 0 h24 v24 h-24 v-24"
-                  fill="white" fill-rule="evenodd" transform="translate(24)" />
+                  fill="white"
+                  fill-rule="evenodd"
+                  transform="translate(24)"
+                />
                 <path
                   d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z M0 0 h24 v24 h-24 v-24"
-                  fill="white" fill-rule="evenodd" transform="translate(48)" />
+                  fill="white"
+                  fill-rule="evenodd"
+                  transform="translate(48)"
+                />
                 <path
                   d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z M0 0 h24 v24 h-24 v-24"
-                  fill="white" fill-rule="evenodd" transform="translate(72)" />
+                  fill="white"
+                  fill-rule="evenodd"
+                  transform="translate(72)"
+                />
                 <path
                   d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z M0 0 h24 v24 h-24 v-24"
-                  fill="white" fill-rule="evenodd" transform="translate(96)" />
+                  fill="white"
+                  fill-rule="evenodd"
+                  transform="translate(96)"
+                />
               </symbol>
             </defs>
           </svg>
           <div class="evaluation">
             <div class="rating">
               <!--   <div class="rating-bg" style="width: 90%;"></div> -->
-              <progress class="rating-bg" :value="resto_data.rating" max="5"></progress>
+              <progress
+                class="rating-bg"
+                :value="resto_data.rating"
+                max="5"
+              ></progress>
               <svg>
                 <use xlink:href="#fivestars" />
               </svg>
             </div>
             <span class="tag is-warning">{{ resto_data.rating }}</span>
             <span :class="'tag is-' + color_map[resto_data.price_range - 1]">
-              {{ "$".repeat(resto_data.price_range) }}</span>
+              {{ "$".repeat(resto_data.price_range) }}</span
+            >
           </div>
-
         </div>
       </div>
 
-
       <div class="box" id="second-box">
-        <div>
-          <h1 class="title">About us</h1>
-          <div class="container resto-info">
+        <h1 class="title">About us</h1>
+        <div class="resto-info">
+          <div class="first-part">
             <Schedule></Schedule>
             <div class="box">
               <table class="table is-hoverable">
                 <thead>
                   <th><abbr title="Weekday">Tags </abbr></th>
                 </thead>
-                <tbody v-for="tag in  json.genres" :key="tag">
+                <tbody v-for="tag in json.genres" :key="tag">
                   <tr>
-                    <th id="tags" class="tag is-info is-medium"> {{ tag }}</th>
+                    <th id="tags" class="tag is-info is-medium">{{ tag }}</th>
                   </tr>
                 </tbody>
               </table>
@@ -65,38 +82,48 @@
                 </thead>
                 <tbody>
                   <tr>
-                    <th> {{ resto_data.tel }}</th>
+                    <th>{{ resto_data.tel }}</th>
                   </tr>
                 </tbody>
               </table>
             </div>
-            <Carousel id="gallery" :items-to-show="1" :wrap-around="true" v-model="currentSlide" :settings="settings">
-              <Slide v-for="pic in json.pictures" :key="pic">
-                <div class="carousel__item">
-                  <div class="card">
-                    <figure>
-                      <img :src="pic" alt="" />
-                    </figure>
-                  </div>
-                </div>
-              </Slide>
-            </Carousel>
           </div>
+          <Carousel
+            id="gallery"
+            :items-to-show="1"
+            :wrap-around="true"
+            v-model="currentSlide"
+            :settings="settings"
+          >
+            <Slide v-for="pic in json.pictures" :key="pic">
+              <div class="carousel__item">
+                <div class="card">
+                  <figure class="image is-4by3">
+                    <img :src="pic" alt="" />
+                  </figure>
+                </div>
+              </div>
+            </Slide>
+          </Carousel>
         </div>
-
-
       </div>
       <div class="container">
         <MapLocation></MapLocation>
       </div>
 
-
-      <Carousel :wrap-around="true" :settings="settings" :breakpoints="breakpoints">
+      <Carousel
+        :wrap-around="true"
+        :settings="settings"
+        :breakpoints="breakpoints"
+      >
         <Slide key="slide">
           <div class="carousel__item">
             <div class="card">
               <figure class="image is-4by3">
-                <img src="https://i.pinimg.com/236x/83/c1/e7/83c1e7c64211f263f588a2f74dd309c6.jpg" alt="" />
+                <img
+                  src="https://i.pinimg.com/236x/83/c1/e7/83c1e7c64211f263f588a2f74dd309c6.jpg"
+                  alt=""
+                />
               </figure>
             </div>
             <div class="card-content is-overlay">
@@ -110,8 +137,11 @@
         <Slide key="slide">
           <div class="carousel__item">
             <div class="card">
-              <figure class="image is-4by3">
-                <img src="https://i.pinimg.com/564x/08/25/38/082538afc45124898e4a792a5250d352.jpg" alt="" />
+              <figure class="">
+                <img
+                  src="https://i.pinimg.com/564x/08/25/38/082538afc45124898e4a792a5250d352.jpg"
+                  alt=""
+                />
               </figure>
             </div>
             <div class="card-content is-overlay">
@@ -126,7 +156,10 @@
           <div class="carousel__item">
             <div class="card">
               <figure class="image is-4by3">
-                <img src="https://i.pinimg.com/564x/95/19/fb/9519fb6c2a9576b4017356c29fb15f93.jpg" alt="" />
+                <img
+                  src="https://i.pinimg.com/564x/95/19/fb/9519fb6c2a9576b4017356c29fb15f93.jpg"
+                  alt=""
+                />
               </figure>
             </div>
             <div class="card-content is-overlay">
@@ -141,7 +174,10 @@
           <div class="carousel__item">
             <div class="card">
               <figure class="image is-4by3">
-                <img src="https://i.pinimg.com/564x/9f/2f/6b/9f2f6bd333752f91e23514ea1a946e3c.jpg" alt="" />
+                <img
+                  src="https://i.pinimg.com/564x/9f/2f/6b/9f2f6bd333752f91e23514ea1a946e3c.jpg"
+                  alt=""
+                />
               </figure>
             </div>
             <div class="card-content is-overlay">
@@ -156,7 +192,10 @@
           <div class="carousel__item">
             <div class="card">
               <figure class="image is-4by3">
-                <img src="https://i.pinimg.com/564x/98/10/2c/98102cbaf0d75501f968ece02a83482f.jpg" alt="" />
+                <img
+                  src="https://i.pinimg.com/564x/98/10/2c/98102cbaf0d75501f968ece02a83482f.jpg"
+                  alt=""
+                />
               </figure>
             </div>
             <div class="card-content is-overlay">
@@ -175,7 +214,6 @@
       </Carousel>
     </div>
   </div>
-
 </template>
 
 <script setup>
@@ -208,20 +246,19 @@ const breakpoints = {
     itemsToShow: 4.5,
   },
 };
-
 </script>
 <script>
-import { defineComponent } from 'vue';
-import { Carousel, Slide, Navigation, Pagination } from 'vue3-carousel';
-import Schedule from './Schedule.vue';
-import MapLocation from './MapLocation.vue';
-import json from './hardcoded_resto.json';
-import 'vue3-carousel/dist/carousel.css';
+import { defineComponent } from "vue";
+import { Carousel, Slide, Navigation, Pagination } from "vue3-carousel";
+import Schedule from "./Schedule.vue";
+import MapLocation from "./MapLocation.vue";
+import json from "./hardcoded_resto.json";
+import "vue3-carousel/dist/carousel.css";
 
 json = json.items[0];
 
 export default defineComponent({
-  name: 'Basic',
+  name: "Basic",
   components: {
     Carousel,
     Slide,
@@ -229,18 +266,18 @@ export default defineComponent({
   },
   data: () => ({
     currentSlide: 0,
-    resto_data: json
+    resto_data: json,
   }),
   methods: {
     slideTo(val) {
       this.currentSlide = val;
-    }
-  }
+    },
+  },
 });
 </script>
 
 <style scoped>
-span+span {
+span + span {
   margin-left: 10px;
 }
 
@@ -251,7 +288,6 @@ span+span {
 }
 
 .container {
-
   margin: auto;
 }
 
@@ -262,7 +298,7 @@ span+span {
 .slider_container {
   width: 100%;
   margin: auto;
-  border: 3px solid #73AD21;
+  border: 3px solid #73ad21;
   flex-shrink: 1;
 }
 
@@ -289,11 +325,24 @@ span+span {
   justify-content: center;
   flex-direction: column;
   flex-shrink: 1;
+  max-height: 350px;
+  max-width: 500px;
+}
 
+@media screen and (max-width: 1216px) {
+  .resto-info {
+    flex-direction: column;
+  }
+}
+
+.first-part {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
 }
 
 .resto-info {
-  display: inline;
+  display: flex;
 }
 
 .carousel__prev {
@@ -301,33 +350,24 @@ span+span {
   border: 5px solid white;
   flex-shrink: 1;
 }
-
-
 </style>
 <style scoped>
 .carousel__slide {
   padding: 25px;
   opacity: 0.25;
   transform: scale(0.8);
-  
-
 }
 
 .carousel__slide--prev {
-
   opacity: 0.5;
-
 }
 
 .carousel__slide--next {
-
   opacity: 0.5;
-
 }
 
 .carousel__slide--active {
-
-  opacity: 1.0;
+  opacity: 1;
   transform: scale(1);
 }
 

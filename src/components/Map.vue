57ad7ca58b170a730690ffd9be59cfcf5ee5e40d
Alex Prudhomme <prudhomme.alex42@gmail.com>
2023-02-11 16:55:37 -0500

    add map with directions

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,35 +1,37 @@
 <template>
   <div class="box mb-6">
-    <div
-      class="is-flex is-flex-direction-row is-justify-content-space-around pb"
-    >
-      <div class="is-flex is-flex-direction-row">
-        <input class="input is-info m-1" placeholder="enter adress" />
-        <button class="button is-info m-1">Find me</button>
-      </div>
-      <div class="">
-        <button class="button is-primary m-1">Show directions</button>
-        <button class="button is-primary m-1">Hide directions</button>
-      </div>
-    </div>
-    <div id="map" style="height: 500px; width: 100%"></div>
+    <div id="map" style="height: 600px; width: 100%"></div>
   </div>
 </template>
 
 <script setup>
-import mapboxgl from "mapbox-gl";
+import json from "../dummy_jsons/hardcoded_resto.json";
 import { onMounted } from "vue";
+const restaurantCoordinates = json.items[0].location.coordinates;
+const restaurantAdress = json.items[0].address;
 onMounted(() => {
   mapboxgl.accessToken =
     "pk.eyJ1IjoicGFsZXg5OTkiLCJhIjoiY2xlMDl4YWMwMG42OTN2bzgwbjd1bW82aCJ9.fH1lEdpJsNacjxdL8RlYog";
   const map = new mapboxgl.Map({
-    container: "map", // container ID
-    style: "mapbox://styles/mapbox/streets-v12", // style URL
-    center: [-74.5, 40], // starting position [lng, lat]
-    zoom: 9, // starting zoom
+    container: "map",
+    // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+    style: "mapbox://styles/mapbox/streets-v12",
+    center: restaurantCoordinates,
+    zoom: 12,
   });
+  const marker = new mapboxgl.Marker({
+    color: "#8F3BDE",
+  })
+    .setLngLat(restaurantCoordinates)
+    .addTo(map);
+  const directions = new MapboxDirections({
+    accessToken: mapboxgl.accessToken,
+  });
+  map.addControl(directions, "top-left");
+  directions.setDestination(restaurantAdress);
 });
 </script>
+
 <style scoped>
 .pb {
   padding-bottom: 1.25rem;

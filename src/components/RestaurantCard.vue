d274f4657ee38ede793c0e56cb9ceda7f9d62d7e
Alex Prudhomme <prudhomme.alex42@gmail.com>
2023-02-13 16:47:47 -0500

    fix bigger restaurant adress with tooltip

--- src/components/RestaurantCard.vue
+++ src/components/RestaurantCard.vue
@@ -46,7 +46,10 @@ const color_map = ["success", "primary", "danger", "black"];
       </div>
 
       <!-- Direction button -->
-      <div class="button">{{ address }}</div>
+      <div class="button tooltip">
+        <span class="tooltiptext">{{ address }}</span>
+        {{ address.slice(0, 50) + "..." }}
+      </div>
 
       <!-- Phone button -->
       <div class="button">{{ tel }}</div>
@@ -130,6 +133,41 @@ export default {
 </script>
 
 <style scoped>
+.tooltip {
+  position: relative;
+  display: inline-block;
+}
+
+.tooltip .tooltiptext {
+  visibility: hidden;
+  width: fit-content + 20px;
+  background-color: #555;
+  color: #fff;
+  text-align: center;
+  padding: 5px 0;
+  border-radius: 6px;
+  position: absolute;
+  z-index: 1;
+  bottom: 125%;
+  left: 50%;
+  margin-left: -60px;
+  opacity: 0;
+  transition: opacity 0.3s;
+}
+.tooltip .tooltiptext::after {
+  content: "";
+  position: absolute;
+  top: 100%;
+  left: 50%;
+  margin-left: -5px;
+  border-width: 5px;
+  border-style: solid;
+  border-color: #555 transparent transparent transparent;
+}
+.tooltip:hover .tooltiptext {
+  visibility: visible;
+  opacity: 1;
+}
 .card {
   height: 720px;
 }

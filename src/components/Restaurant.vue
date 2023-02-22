4fd17c78d3eb326af7b57ac965279f1ca7ffe9fe
Alex Prudhomme <prudhomme.alex42@gmail.com>
2023-02-21 21:49:34 -0500

    add buttons for modal in restaurant page

--- src/components/Restaurant.vue
+++ src/components/Restaurant.vue
@@ -2,61 +2,70 @@
   <div class="resto">
     <div class="container is-max-widescreen">
       <br /><br />
-      <div class="box">
-        <h1 class="title">{{ resto_data.name }}</h1>
-        <div class="rating-container">
-          <svg style="display: none">
-            <defs>
-              <symbol id="fivestars">
-                <path
-                  d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z M0 0 h24 v24 h-24 v-24"
-                  fill="white"
-                  fill-rule="evenodd"
-                />
-                <path
-                  d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z M0 0 h24 v24 h-24 v-24"
-                  fill="white"
-                  fill-rule="evenodd"
-                  transform="translate(24)"
-                />
-                <path
-                  d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z M0 0 h24 v24 h-24 v-24"
-                  fill="white"
-                  fill-rule="evenodd"
-                  transform="translate(48)"
-                />
-                <path
-                  d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z M0 0 h24 v24 h-24 v-24"
-                  fill="white"
-                  fill-rule="evenodd"
-                  transform="translate(72)"
-                />
-                <path
-                  d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z M0 0 h24 v24 h-24 v-24"
-                  fill="white"
-                  fill-rule="evenodd"
-                  transform="translate(96)"
-                />
-              </symbol>
-            </defs>
-          </svg>
-          <div class="evaluation">
-            <div class="rating">
-              <!--   <div class="rating-bg" style="width: 90%;"></div> -->
-              <progress
-                class="rating-bg"
-                :value="resto_data.rating"
-                max="5"
-              ></progress>
-              <svg>
-                <use xlink:href="#fivestars" />
-              </svg>
+      <div
+        class="box is-flex is-justify-content-space-between"
+        id="name-buttons"
+      >
+        <div>
+          <h1 class="title">{{ resto_data.name }}</h1>
+          <div class="rating-container">
+            <svg style="display: none">
+              <defs>
+                <symbol id="fivestars">
+                  <path
+                    d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z M0 0 h24 v24 h-24 v-24"
+                    fill="white"
+                    fill-rule="evenodd"
+                  />
+                  <path
+                    d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z M0 0 h24 v24 h-24 v-24"
+                    fill="white"
+                    fill-rule="evenodd"
+                    transform="translate(24)"
+                  />
+                  <path
+                    d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z M0 0 h24 v24 h-24 v-24"
+                    fill="white"
+                    fill-rule="evenodd"
+                    transform="translate(48)"
+                  />
+                  <path
+                    d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z M0 0 h24 v24 h-24 v-24"
+                    fill="white"
+                    fill-rule="evenodd"
+                    transform="translate(72)"
+                  />
+                  <path
+                    d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z M0 0 h24 v24 h-24 v-24"
+                    fill="white"
+                    fill-rule="evenodd"
+                    transform="translate(96)"
+                  />
+                </symbol>
+              </defs>
+            </svg>
+            <div class="evaluation">
+              <div class="rating">
+                <!--   <div class="rating-bg" style="width: 90%;"></div> -->
+                <progress
+                  class="rating-bg"
+                  :value="resto_data.rating"
+                  max="5"
+                ></progress>
+                <svg>
+                  <use xlink:href="#fivestars" />
+                </svg>
+              </div>
+              <span class="tag is-warning">{{ resto_data.rating }}</span>
+              <span :class="'tag is-' + color_map[resto_data.price_range - 1]">
+                {{ "$".repeat(resto_data.price_range) }}</span
+              >
             </div>
-            <span class="tag is-warning">{{ resto_data.rating }}</span>
-            <span :class="'tag is-' + color_map[resto_data.price_range - 1]">
-              {{ "$".repeat(resto_data.price_range) }}</span
-            >
           </div>
+        </div>
+        <div class="is-flex is-flex is-align-items-center">
+          <button class="button is-primary mr-3 is-large">Rate</button>
+          <button class="button is-primary is-large">Add to favorite</button>
         </div>
       </div>
 
@@ -247,6 +256,9 @@ span + span {
   max-height: 350px;
   max-width: 500px;
 }
+#name-buttons {
+  flex-direction: row;
+}
 
 @media screen and (max-width: 1216px) {
   .resto-info {
@@ -256,6 +268,9 @@ span + span {
 
 @media screen and (max-width: 500px) {
   .first-part {
+    flex-direction: column;
+  }
+  #name-buttons {
     flex-direction: column;
   }
 }

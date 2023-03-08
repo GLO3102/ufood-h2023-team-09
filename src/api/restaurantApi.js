4337d5d4ae122a891107a102a2f1173d12c4d8af
Samuel Côté <122579295+cnator1@users.noreply.github.com>
2023-03-07 19:49:51 -0500

    Api pour restaurant + gestion erreur (#32)

--- src/api/restaurantApi.js
+++ src/api/restaurantApi.js
@@ -7,5 +7,20 @@ export const getRestaurantsByPage = async (page) => {
           'Content-Type': 'application/json'
         },
       })
+    if (response.status !== 200) {
+        throw new Error(`Something went wrong : request returned status ${response.status}...`);
+    }
       return response.json();
+}
+
+
+export const getRestaurantByID = async (id) => {
+  const response = await fetch(`${URL}/${id}`, {
+      Method: 'GET',
+    })
+    if (response.status !== 200) {
+      throw new Error(`Something went wrong : request returned status ${response.status}...`);
+  }
+    const data = await response.json();
+    return data;
 }

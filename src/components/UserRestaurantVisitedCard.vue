66c7dc65035325bba56db96c11d6fd6f765a513f
Alex Prudhomme <prudhomme.alex42@gmail.com>
2023-03-15 13:11:20 -0400

    start of read only modal

--- src/components/UserRestaurantVisitedCard.vue
+++ src/components/UserRestaurantVisitedCard.vue
@@ -1,27 +1,38 @@
 <template>
-  <div class="card">
-    <!--image-->
-    <div class="card-image">
-      <figure class="image is-4by3">
-        <img :src="restaurantPic[0]" alt="restaurant picture" />
-      </figure>
+  <div>
+    <VisitModalReadOnly
+      v-if="showVisitModal"
+      @close="closeVisitModal"
+      :id="restaurantId"
+      :name="restaurantName"
+    />
+    <div @click="openVisitModal">
+      <div class="card">
+        <!--image-->
+        <div class="card-image">
+          <figure class="image is-4by3">
+            <img :src="restaurantPic[0]" alt="restaurant picture" />
+          </figure>
+        </div>
+        <!--fin image-->
+        <!-- debut number visits-->
+        <div class="card-content is-overlay">
+          <span class="tag is-primary is-size-5">{{ numberVisits }} </span>
+        </div>
+        <!--end number visits-->
+        <!-- debut name -->
+        <div class="card-content slider-text">
+          <div class="is-size-5 box">{{ restaurantName }}</div>
+        </div>
+        <!--end name-->
+      </div>
     </div>
-    <!--fin image-->
-    <!-- debut number visits-->
-    <div class="card-content is-overlay">
-      <span class="tag is-primary is-size-5">{{ numberVisits }} </span>
-    </div>
-    <!--end number visits-->
-    <!-- debut name -->
-    <div class="card-content slider-text">
-      <div class="is-size-5 box">{{ restaurantName }}</div>
-    </div>
-    <!--end name-->
   </div>
 </template>
 
 <script>
 import { getRestaurantById } from "../api/userApi.js";
+import VisitModalReadOnly from "./VisitModalReadOnly.vue";
 export default {
   props: ["numberVisits", "restaurantId"],
 
@@ -29,10 +40,22 @@ export default {
     restaurantName: "",
     restaurantPic: "",
   }),
-  async created(){
+  async created() {
     const restaurant = await getRestaurantById(this.restaurantId);
-      this.restaurantName = restaurant.name;
-      this.restaurantPic = restaurant.pictures;
-  }
+    this.restaurantName = restaurant.name;
+    this.restaurantPic = restaurant.pictures;
+  },
+};
+</script>
+<script setup>
+import { ref } from "vue";
+
+let showVisitModal = ref(false);
+
+const openVisitModal = () => {
+  showVisitModal.value = true;
+};
+const closeVisitModal = () => {
+  showVisitModal.value = false;
 };
 </script>
